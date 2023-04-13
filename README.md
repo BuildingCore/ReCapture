f1fe7e0fabbe601c917c896833d97c2af6cae051
KeyShaun Ward <kward@buildingcore-studio.com>
2023-04-12 22:01:08 -0400

    Initial setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Recapture Web Application
+---
+
+### Developed by BuildingCore Studio, LLC.
+#### Arthurs: KeyShaun Ward & Nicole Smirnoff
+
+---
+
+### Clone Repository and install dependencies
+
+```bash
+npm i express axios forge-apis aws-sdk
+npm i nodemon -D
+```
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 8080
+
+//Middleware
+
+//Public Routes
+
+//Private Router - App
+
+//Private Router - SDK & API 
+
+//Server Listening on PORT 8080
+app.listen(PORT, () => {
+    console.log(`The server is running on PORT ${PORT}`)
+})
